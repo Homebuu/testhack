ee5bcad1d89fd2661a776e061ba25cd76130a074
Chaipat <87646794+Homebuu@users.noreply.github.com>
2026-03-30 00:06:01 +0700

    Update testproject.lua

--- testproject.lua
+++ testproject.lua
@@ -73,6 +73,8 @@ local playerData = {}
 local flingAllEnabled = false
 local pDropdown = nil
 
+game:GetService("TextChatService"):WaitForChild("TextChannels"):WaitForChild("RBXGeneral"):SendAsync("สวัสดี, ฉันรู้สึกดีมากๆ มันเป็นแมพที่ดี <3")
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 if game.PlaceId == 142823291 then 
 	local remote = ReplicatedStorage
